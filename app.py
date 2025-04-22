ec683a9a0648d0e18a6c3e91f046a02509837be0
chase2981 <chasewgibbons@gmail.com>
2025-04-22 15:03:14 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import snowflake.connector
+import os
+import pandas as pd
+
+app = FastAPI()
+
+def get_snowflake_data():
+    conn = snowflake.connector.connect(
+        user=os.getenv("SNOWFLAKE_USER"),
+        password=os.getenv("SNOWFLAKE_PASSWORD"),
+        account=os.getenv("SNOWFLAKE_ACCOUNT"),
+        warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
+        database=os.getenv("SNOWFLAKE_DATABASE"),
+        schema=os.getenv("SNOWFLAKE_SCHEMA")
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM vw_customer_summary")
+    df = pd.DataFrame.from_records(cursor.fetchall(), columns=[col[0] for col in cursor.description])
+    cursor.close()
+    conn.close()
+    return df.to_dict(orient="records")
+
+@app.get("/data")
+def read_data():
+    return get_snowflake_data()
